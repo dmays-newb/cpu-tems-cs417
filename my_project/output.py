55f385f76759f91ec80f8fe01d2ffd6e5148d52c
Dustin Mays <drm2010.dustin@gmail.com>
2022-03-28 15:45:26 -0400

    Refined output

--- my_project/output.py
+++ my_project/output.py
@@ -10,8 +10,14 @@ def create_outfile(arr, core):
 
     file_name = "core_" + str(core) + ".txt"
     out = open(file_name, "w")
+    time_tracker = 0
     for element in arr:
-        # print(element)
-        out.write(str(element))
+        # print(element) # Debug output
+        line = str(time_tracker*30) + "     <= x <     " +\
+               str(time_tracker*30+30) + ";" +\
+               "      y_" + str(time_tracker) + "     =     " +\
+               str(element) + ";   " + "algorithm"
+        out.write(line)
+        time_tracker += 1
         out.write('\n')
     out.close()
